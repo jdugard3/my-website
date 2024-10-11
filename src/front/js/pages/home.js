ed14be03bda460ac8348863755563753c86128bf
James Dugard <161103530+jdugard3@users.noreply.github.com>
2024-10-10 23:34:35 -0400

    starting this thing out, finally got it running

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,15 +1,13 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-
-	return (
-		<div className="text-center mt-5">
-			<h1>This is my Home Page</h1>
-			
-		</div>
-	);
+  const { store, actions } = useContext(Context);
+  console.log("Hello World!");
+  return (
+    <div className="text-center mt-5">
+      <h1>Hello World!</h1>
+    </div>
+  );
 };
